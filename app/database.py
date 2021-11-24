f14c10b995ce1ca46cda8ee37a5a9ab6ede108a9
Stiffjobs <52703319+Stiffjobs@users.noreply.github.com>
2021-11-24 17:14:27 +0800

    try

--- app/database.py
+++ app/database.py
@@ -1,18 +1,17 @@
-from sqlalchemy import create_engine, engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from psycopg2.extras import RealDictCursor
 import psycopg2
+from psycopg2.extras import RealDictCursor
 import time
-from app.config import settings
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
@@ -26,17 +25,14 @@ def get_db():
 
 
 # while True:
+
 #     try:
-#         conn = psycopg2.connect(host='localhost', database='postgres', user='stiffjobs',
-#                                 password='0306chencharles', cursor_factory=RealDictCursor)
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                                 password='password123', cursor_factory=RealDictCursor)
 #         cursor = conn.cursor()
-#         print(
-#             "database connection was successful!")
+#         print("Database connection was succesfull!")
 #         break
 #     except Exception as error:
-#         print(
-#             "connecting to database failed")
-#         print(
-#             "Error: ", error)
-#         time.sleep(
-#             3)
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
