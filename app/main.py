a35b4739c69df2800ff11cba645af64f02c5e2f9
Stiffjobs <52703319+Stiffjobs@users.noreply.github.com>
2021-11-24 17:46:26 +0800

    try

--- app/main.py
+++ app/main.py
@@ -35,7 +35,8 @@ app.add_middleware(
 def root():
     return {
         "message": {
-            "my value": f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
-            "correct_value": SQLALCHEMY_DATABASE_URL
+            # "my value": f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
+            # "correct_value": SQLALCHEMY_DATABASE_URL
+            "hello world",
         }
     }
