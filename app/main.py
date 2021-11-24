70a9ba448668a05f0d5355719cdf3c34df7f112f
Stiffjobs <52703319+Stiffjobs@users.noreply.github.com>
2021-11-25 01:04:45 +0800

    remove credentials

--- app/main.py
+++ app/main.py
@@ -28,8 +28,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
 
 @app.get("/")
 def root():
