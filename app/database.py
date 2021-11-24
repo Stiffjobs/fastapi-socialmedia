fd2f788edd7b040113dd9e6bf8dad55229b2dac3
Stiffjobs <52703319+Stiffjobs@users.noreply.github.com>
2021-11-24 17:21:18 +0800

    try

--- app/database.py
+++ app/database.py
@@ -6,8 +6,8 @@ from psycopg2.extras import RealDictCursor
 import time
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = 'postgresql://hdcbevzsflohoz:f91dbfe27eca6b450a2b51371e5139958fc2f13d8bba18b8c7a337b53be07977@ec2-3-209-226-234.compute-1.amazonaws.com:5432/ddrb6s6frec1dg'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
